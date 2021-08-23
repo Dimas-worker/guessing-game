149f66076c219ab5c6b8e4c2cf0695f23665968c
Dmitry_Petralai <pdo.hardwork@yandex.ru>
2021-08-23 20:06:30 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.mid = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.mid = Math.ceil((this.min + this.max) / 2);
     }
 
     lower() {
-
+        this.setRange(this.min, this.mid);
     }
 
     greater() {
-
+        this.setRange(this.mid, this.max);
     }
 }
 
